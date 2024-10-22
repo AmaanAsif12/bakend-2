3db5c92548d58be1fcd9e6896ade3e703a700cf0
Amaan Asif <amaanasif858gmail.com>
2024-10-22 04:19:17 -0700

    first commit

---
+++ index.mjs
@@ -0,0 +1,20 @@
+import express from 'express';
+import router from './Router/index.js';
+import mongoose from './config/db.js';
+import cors from 'cors'
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ message: "Hello from the server !" })
+})
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+    mongoose.connection.once('open', () => {
+        console.log('db connected successfully!')
+    });
+});
+app.use(cors());
+app.use(express.json());
+// Use the router
+app.use('/', router);
